a636c6718f547c5d11f8d7f8809cd16f53807662
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2021-12-18 23:36:23 -0300

    Script

--- script_graficos_animados.R
+++ script_graficos_animados.R
@@ -34,20 +34,24 @@ p <- ggplot(d, aes(x = Year, y = media, fill = Year)) +
   geom_point(position = position_dodge(.9), size = 1.7) +
   geom_errorbar(aes(x = Year, ymin = media - se, ymax = media + se), 
                 width = 0.14, position = position_dodge(.9), size = 0.9) +
-  scale_color_viridis_d() +
-  labs(x = "Tempo (anos)", y = "Acesso à internet no mundo") +
+  scale_fill_viridis() +
+  labs(x = "Tempo (anos)", y = "Acesso à internet no mundo",
+       caption = "Fonte dos dados: Our World in Data") +
   theme_bw(base_size = 15) +
   theme(legend.position = "none") 
-p
+p 
 
 p1 <- ggplot(dados, 
   aes(x = Year, y = Individuals.using.the.Internet....of.population.,
       group = Entity, color = Entity)) +
   geom_line(size = 0.68) +
   scale_color_viridis_d() +
-  labs(x = "Tempo (anos)", y = "Acesso à internet no mundo") +
+  labs(x = "Tempo (anos)", y = "Acesso à internet no mundo",
+       caption = "Fonte dos dados: Our World in Data") +
   theme_bw(base_size = 15) +
   theme(legend.position = "none")
 p1
 
-p1 + geom_point() + transition_reveal(Year)
+gr <- p1 + geom_point() + transition_reveal(Year)
+
+anim_save("gr.gif", width = 10, height = 10)
